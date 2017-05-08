fe92e09c6eff84d4b3108bf822063415a4dc2ccb
abner <fantasy@Fantasy-iMac.local>
2017-05-08 18:46:05 +0800

    修改Demo

--- ZJUnFoldView/DetailViewController.m
+++ ZJUnFoldView/DetailViewController.m
@@ -24,19 +24,29 @@
     [super viewDidLoad];
     
     self.title = self.dict[@"title"];
-    self.lineSpacingLabel.text = [NSString stringWithFormat:@"行间距：%.1f", ((NSNumber *)self.dict[@"lineSpacing"]).floatValue];
-    self.foldLinesLabel.text = [NSString stringWithFormat:@"默认折叠：%ld", (long)((NSNumber *)self.dict[@"foldLines"]).integerValue];
-    self.isDefaultUnFoldLabel.text = [NSString stringWithFormat:@"是否展开：%@", ((NSNumber *)self.dict[@"isDefaultUnFold"]).boolValue ? @"展开" : @"不展开"];
+    
+    CGFloat lineSpacing = ((NSNumber *)self.dict[@"lineSpacing"]).floatValue;
+    NSInteger foldLines = ((NSNumber *)self.dict[@"foldLines"]).integerValue;
+    BOOL isDefaultUnFold = ((NSNumber *)self.dict[@"isDefaultUnFold"]).boolValue;
     NSInteger locationValue = ((NSNumber *)self.dict[@"location"]).integerValue;
+    self.lineSpacingLabel.text = [NSString stringWithFormat:@"行间距：%.1f", lineSpacing];
+    self.foldLinesLabel.text = [NSString stringWithFormat:@"默认折叠：%ld", (long)foldLines];
+    self.isDefaultUnFoldLabel.text = [NSString stringWithFormat:@"是否展开：%@", isDefaultUnFold ? @"展开" : @"不展开"];
     self.locationLabel.text = [NSString stringWithFormat:@"展开按钮位置：%@", (locationValue == 0) ? @"左边" : ((locationValue == 1) ? @"中间" : @"右边")];
     
     
     ZJUnFoldAttributedString *unFoldAttrStr = nil;
-    NSString *content = @"人生，如一幅春夏秋冬的景象，岁月，像一趟穿越四季的列车，我们都在路上。际遇是沿途的风光，无论我们是无视或是欣赏，终究都成为匆匆的过往。珍惜吧，珍惜会让生命中的美好增加一些重量。豁然吧，豁然是因为明白一切的风霜仅仅是过场。";
+    NSMutableString *content = [[NSMutableString alloc] initWithString:@"人生，如一幅春夏秋冬的景象，岁月，像一趟穿越四季的列车，我们都在路上。际遇是沿途的风光，无论我们是无视或是欣赏，终究都成为匆匆的过往。珍惜吧，珍惜会让生命中的美好增加一些重量。豁然吧，豁然是因为明白一切的风霜仅仅是过场。"];
     if ([self.title isEqualToString:@"默认配置初始化"]) {
         // 1.获取属性字符串：默认配置
         unFoldAttrStr = [ZJUnFoldAttributedString defaultConficAttributedString:content];
     } else {
+        
+        // 注意：在4.7寸屏幕下由于测试文字内容长度不够，故增加一段字符来模拟这种情景，仅仅只用于测试
+        if ([self.title isEqualToString:@"展开时展开按钮位置：左边"]) {
+            [content appendString:@"豁然是因为明白一切的风霜仅仅"];
+        }
+        
         // 1.获取属性字符串：自定义内容和属性
         unFoldAttrStr = [[ZJUnFoldAttributedString alloc] initWithContent:content
                                                               contentFont:[UIFont systemFontOfSize:12.0f]
@@ -45,11 +55,11 @@
                                                                foldString:@"[收回]"
                                                                unFoldFont:[UIFont systemFontOfSize:12.0f]
                                                               unFoldColor:[ZJUnFoldView colorWithHexString:@"#dd4991"]
-                                                              lineSpacing:7.0f];
+                                                              lineSpacing:lineSpacing];
     }
     
     // 2.添加展开视图
-    ZJUnFoldView *unFoldView = [[ZJUnFoldView alloc] initWithAttributedString:unFoldAttrStr maxWidth:[UIScreen mainScreen].bounds.size.width - 60.0f isDefaultUnFold:NO foldLines:3 location:UnFoldButtonLocationRight];
+    ZJUnFoldView *unFoldView = [[ZJUnFoldView alloc] initWithAttributedString:unFoldAttrStr maxWidth:[UIScreen mainScreen].bounds.size.width - 60.0f isDefaultUnFold:isDefaultUnFold foldLines:foldLines location:locationValue];
     unFoldView.frame = CGRectMake(30, CGRectGetMaxY(self.locationLabel.frame) + 30.0f, unFoldView.frame.size.width, unFoldView.frame.size.height);
     unFoldView.backgroundColor = [UIColor orangeColor];
     [self.view addSubview:unFoldView];
